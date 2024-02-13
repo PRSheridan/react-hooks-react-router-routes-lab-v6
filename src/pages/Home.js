234b441e24d3f5b1d9e82079fce9e75bded5acae
Phil <philrsheridan@gmail.com>
2024-02-13 16:56:11 -0500

    rendering Movie pages broken

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,29 @@
 import { useEffect, useState } from "react";
 import Navbar from "../components/NavBar"
+import MovieCard from "../components/MovieCard";
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/`)
+    .then(r => r.json())
+    .then(data => setMovies(data))
+    .catch (error => console.log(error))
+  }, []);
+
+  const movieList = movies.map(movie => {
+    return <MovieCard key={movie.id} movie={movie} />
+  })
+
   return (
     <>
+    <h1>Home Page</h1>
       <header>
         <Navbar />
       </header>
       <main>
-        {/* Info goes here! */}
+        {movieList}
       </main>
     </>
   );
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,13 +1,25 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function Movie() {
+  const [movie, setMovie] = useState({});
+  const params = useParams();
+  const movieId = params.id;
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${movieId}`)
+    .then(r => r.json())
+    .then(data => setMovie(data))
+    .catch(error => console.error(error))
+  }, [movieId])
+
   return (
     <>
-      <header>
-        {/* What component should go here? */}
-      </header>
+      <header></header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>{movie.time}</p>
+        {"test"}
       </main>
     </>
   );
